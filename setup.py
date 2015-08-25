97999c994a0ff96680a05c312e5644dba300ce1e
Ken Dreyer <kdreyer@redhat.com>
2015-08-25 09:34:42 -0600

    make "python setup.py test" work
    Integrate pytest into setuptools, a la
    https://pytest.org/latest/goodpractises.html

--- setup.py
+++ setup.py
@@ -1,6 +1,26 @@
+import sys
 from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
 
 version = '0.0.1'
+
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        #import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main('tests', self.pytest_args)
+        sys.exit(errno)
 
 setup(name="jenkins-job-wrecker",
       version=version,
@@ -25,4 +45,6 @@ setup(name="jenkins-job-wrecker",
             'jjwrecker = jenkins_job_wrecker.cli:main',
             ],
       },
+      tests_require=['pytest'],
+      cmdclass = {'test': PyTest},
 )
