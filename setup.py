88f00bb3943d3c21dcfb6c340259dfa075eb1a3b
Ken Dreyer <kdreyer@redhat.com>
2015-10-26 14:34:24 -0600

    version 1.0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import sys
 from setuptools import setup, find_packages
 from setuptools.command.test import test as TestCommand
 
-version = '1.0.2'
+version = '1.0.3'
 
 class PyTest(TestCommand):
     user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
