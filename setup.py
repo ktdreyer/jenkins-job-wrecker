4c68f8b364351deb373cd5414e990a57d05000e0
Ken Dreyer <kdreyer@redhat.com>
2016-08-04 16:02:10 -0600

    version 1.0.7

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import sys
 from setuptools import setup, find_packages, Command
 from setuptools.command.test import test as TestCommand
 
-version = '1.0.6'
+version = '1.0.7'
 
 
 class PyTest(TestCommand):
