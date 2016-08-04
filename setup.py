561e78b099d12d3e5ed007fbb3e2811bb4ba41b9
Ken Dreyer <kdreyer@redhat.com>
2016-08-04 16:04:46 -0600

    version 1.0.8
    I neglected to push the earlier 1.0.7 commit, but somehow I'd already
    pushed a 1.0.7 tag. Welp, releasing is hard. Let's just go to 1.0.8.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import sys
 from setuptools import setup, find_packages, Command
 from setuptools.command.test import test as TestCommand
 
-version = '1.0.7'
+version = '1.0.8'
 
 
 class PyTest(TestCommand):
