6fc46b04204d16752428f1062fdba96fb5b835dc
Ken Dreyer <kdreyer@redhat.com>
2016-08-22 17:25:04 -0600

    setup.py: push master branch
    This saves a manual step when releasing.

--- setup.py
+++ setup.py
@@ -49,6 +49,11 @@ class ReleaseCommand(Command):
         print ' '.join(cmd)
         subprocess.check_call(cmd)
 
+        # Push branch to origin remote
+        cmd = ['git', 'push', 'origin', 'master']
+        print ' '.join(cmd)
+        subprocess.check_call(cmd)
+
         # Push package to pypi
         cmd = ['python', 'setup.py', 'sdist', 'upload']
         if self.sign:
