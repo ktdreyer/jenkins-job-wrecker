b751503cad081772f239ba242d17085408adb276
Ken Dreyer <ktdreyer@ktdreyer.com>
2017-05-05 08:47:39 -0600

    version 1.4.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import sys
 from setuptools import setup, find_packages, Command
 from setuptools.command.test import test as TestCommand
 
-version = '1.4.2'
+version = '1.4.3'
 
 
 class PyTest(TestCommand):
