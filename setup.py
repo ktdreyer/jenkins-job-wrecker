887661dd135ceb3e5c8bd8dbdb41d2955967d783
Adam Romanek <aromanek.contractor@libertyglobal.com>
2020-05-08 10:15:58 +0200

    Fix pytest invocation via 'python setup.py test'

--- setup.py
+++ setup.py
@@ -123,7 +123,7 @@ class PyTest(TestCommand):
 
     def run_tests(self):
         import pytest
-        errno = pytest.main('tests', self.pytest_args)
+        errno = pytest.main(self.pytest_args)
         sys.exit(errno)
 
 
