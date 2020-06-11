e7f3edd912bcf731de55ec874d74b06f32602fa5
Eren ATAS <eatas.contractor@libertyglobal.com>
2020-06-11 15:31:55 +0200

    Added support for rebuildsettings
    Also added test cases
    
    Signed-off-by: Eren ATAS <eatas.contractor@libertyglobal.com>
    Co-authored-by: Adam Romanek <romanek-adam@users.noreply.github.com>

--- tests/test_modules_properties.py
+++ tests/test_modules_properties.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from jenkins_job_wrecker.cli import get_xml_root
-from jenkins_job_wrecker.modules.properties import authorizationmatrixproperty
+from jenkins_job_wrecker.modules.properties import authorizationmatrixproperty, rebuildsettings
 import os
 
 fixtures_path = os.path.join(os.path.dirname(__file__), 'fixtures', 'properties')
@@ -19,3 +19,18 @@ class TestAuthorizationMatrixProperty(object):
         assert len(jjb_authorization) == 2
         assert jjb_authorization['usera'] == ['job-build', 'job-read']
         assert jjb_authorization['userb'] == ['job-configure', 'job-workspace']
+
+
+class TestRebuildSettings(object):
+    def test_basic(self):
+        filename = os.path.join(fixtures_path, 'rebuild-settings.xml')
+        root = get_xml_root(filename=filename)
+        assert root is not None
+        parent = []
+        rebuildsettings(root, parent)
+        assert len(parent) == 1
+        assert "rebuild" in parent[0]
+        rebuild = parent[0]["rebuild"]
+        assert len(rebuild) == 2
+        assert rebuild["auto-rebuild"] is False
+        assert rebuild["rebuild-disabled"] is False
